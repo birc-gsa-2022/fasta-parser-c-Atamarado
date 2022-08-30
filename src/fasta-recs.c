93f547819aadc413d6fda7366b71796b6d8ed78d
Atamarado <52821119+Atamarado@users.noreply.github.com>
2022-08-30 13:19:21 +0200

    Update work fasta-recs.c

--- src/fasta-recs.c
+++ src/fasta-recs.c
@@ -1,38 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-// Reads a file and puts in a buffer, in an allocated memory space
-// https://stackoverflow.com/questions/3747086/reading-the-whole-text-file-into-a-char-array-in-c
-char* readFile(const char* filename){
+/*
+ * 'slurp' reads the file identified by 'path' into a character buffer
+ * pointed at by 'buf', optionally adding a terminating NUL if
+ * 'add_nul' is true. On success, the size of the file is returned; on
+ * failure, -1 is returned and ERRNO is set by the underlying system
+ * or library call that failed.
+ *
+ * WARNING: 'slurp' malloc()s memory to '*buf' which must be freed by
+ * the caller.
+ */
+long slurp(char const* path, char **buf, bool add_nul)
+{
+    FILE  *fp;
+    size_t fsz;
+    long   off_end;
+    int    rc;
+
+    /* Open the file */
+    fp = fopen(path, "rb");
+    if( NULL == fp ) {
+        return -1L;
+    }
+
+    /* Seek to the end of the file */
+    rc = fseek(fp, 0L, SEEK_END);
+    if( 0 != rc ) {
+        return -1L;
+    }
+
+    /* Byte offset to the end of the file (size) */
+    if( 0 > (off_end = ftell(fp)) ) {
+        return -1L;
+    }
+    fsz = (size_t)off_end;
+    printf("a\n");
+
+    /* Allocate a buffer to hold the whole file */
+    *buf = malloc( fsz+(int)add_nul );
+    if( NULL == *buf ) {
+        return -1L;
+    }
+    printf("a\n");
+
+    /* Rewind file pointer to start of file */
+    rewind(fp);
+
+    /* Slurp file into buffer */
+    if( fsz != fread(*buf, 1, fsz, fp) ) {
+        free(*buf);
+        return -1L;
+    }
     
-    FILE *fp;
-    fp = fopen(filename, "r");
-    if( !fp ) perror("blah.txt"),exit(1);
+    /* Close the file */
+    if( EOF == fclose(fp) ) {
+        free(*buf);
+        return -1L;
+    }
 
-    fseek( fp , 0L , SEEK_END);
-    long lSize = ftell( fp );
-    rewind( fp );
+    if( add_nul ) {
+        /* Make sure the buffer is NUL-terminated, just in case */
+        buf[fsz] = '\0';
+    }
 
-    /* allocate memory for entire content */
-    char *buffer = calloc( 1, lSize+1 );
-    if( !buffer ) fclose(fp),fputs("memory alloc fails",stderr),exit(1);
-
-    /* copy the file into the buffer */
-    if( 1!=fread( buffer , lSize, 1 , fp) )
-    fclose(fp),free(buffer),fputs("entire read fails",stderr),exit(1);
-
-    /* do your work here, buffer is a string contains the whole text */
-
-    fclose(fp);
-    free(buffer);
-
-    return buffer;
-
+    /* Return the file size */
+    return (long)fsz;
 }
 
 int main(int argc, char const *argv[])
 {
+    printf("Holis\n");
     if (argc != 2)
     {
         fprintf(stderr, "Usage: fasta-recs fasta-file\n");
@@ -40,42 +80,51 @@ int main(int argc, char const *argv[])
     }
 
     const char *fasta_fname = argv[1];
-
-    char *content = readFile(fasta_fname);
+    char **pContent;
+    printf("Holis\n");
+    long fileSize = slurp(fasta_fname, pContent, 1);
+    printf("Holis\n");
+    if(fileSize<0){
+        fprintf(stderr, "Error while reading the file\n");
+        exit(2);
+    }
+    char *content = *pContent;
 
     //Parsing into DNA chains
     int pos = 0;
-    const int length = strlen(content);
+
+    char c = content[pos];
 
     // Find every chain
-    while(pos<length){
+    while(c!='\0'){
         //First we have to find '>'
-        char c = content[pos];
-        while(pos < length && c!='>'){
+        while(c!='\0' && c!='>'){
             pos++;
             c = content[pos];
         }
 
         // Skip spaces
-        while(pos < length && c!=' '){
+        while(c!='\0' && c!=' '){
             pos++;
             c = content[pos];
         }
 
         // Find the name
         int nameBegin = pos;
-        while(pos<length && c!=' ' && c!='\n'){
+        while(c!='\0' && c!=' ' && c!='\n'){
             pos++;
             c = content[pos];
         }
 
-        char *name = calloc(pos-nameBegin, sizeof(char));
+        char *name;
+        free(name);
+        name = malloc(pos-nameBegin);
         memcpy(name, &content[nameBegin], pos);
 
         // Find the chain
         int nChain = 0;
         int chainBegin = pos;
-        while(pos<length && c!='>'){
+        while(c!='\0' && c!='>'){
             if(c!=' ' && c!='\n'){
                 nChain++;
             }
@@ -97,6 +146,7 @@ int main(int argc, char const *argv[])
         printf("\n");
     }
 
+    free(pContent);
 
     return 1;
 }
